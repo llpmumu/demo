e795a5d2f282a284286a529a7cf590748e6f0450
llpmumu <824484941@qq.com>
2018-03-06 18:04:06 +0800

    ........

---
+++ src/main/java/com/manshop/bean/Message.java
@@ -0,0 +1,82 @@
+package com.manshop.bean;
+import org.nutz.dao.entity.annotation.*;
+import org.nutz.dao.entity.annotation.Table;
+
+
+@Table("t_msg")
+public class Message {
+    @Id
+    private Integer id;
+    @ColDefine(type = ColType.VARCHAR, width = 100)
+    private String msg;
+    @ColDefine(notNull = true)
+    private Integer sender;
+    @One(field = "sender", key = "id")
+    private User suser;
+    @ColDefine(notNull = true)
+    private Integer receiver;
+    @One(field = "receiver", key = "id")
+    private User ruser;
+    @ColDefine(notNull = true,type = ColType.INT, width = 1)
+    @Default(value = "0")
+    private Integer type;
+
+    public Message() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getSender() {
+        return sender;
+    }
+
+    public void setSender(Integer sender) {
+        this.sender = sender;
+    }
+
+    public User getSuser() {
+        return suser;
+    }
+
+    public void setSuser(User suser) {
+        this.suser = suser;
+    }
+
+    public Integer getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Integer receiver) {
+        this.receiver = receiver;
+    }
+
+    public User getRuser() {
+        return ruser;
+    }
+
+    public void setRuser(User ruser) {
+        this.ruser = ruser;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
