efa1346ad9dfffee5ae7d085d5edb1725bd172af
llpmumu <824484941@qq.com>
2018-03-08 18:09:57 +0800

    ........

--- src/main/java/com/manshop/module/MessageModule.java
+++ src/main/java/com/manshop/module/MessageModule.java
@@ -33,9 +33,28 @@ public class MessageModule {
         return ResponseModel.getCommonSuccessResponseModel(message.getId());
     }
 
-    @At("/getMsg")
+    @At("/getSendMsg")
     @POST
-    public ResponseModel getMsg(Message message) {
+    public ResponseModel getSendMsg(Message message) {
+        List<Message> result = dao.query(Message.class, Cnd.where("sender", "=", message.getSender()).and("receiver","=",message.getReceiver()));
+        for (int i = 0; i < result.size(); i++) {
+            //接受方
+            User rUser = dao.fetch(User.class, result.get(i).getReceiver());
+            result.get(i).setRuser(rUser);
+
+            //发送方
+            User sUser = dao.fetch(User.class, result.get(i).getSender());
+            result.get(i).setSuser(sUser);
+        }
+        sortUtil.mTimeSort(result);
+        if (result.isEmpty())
+            return ResponseModel.getCommonFailedResponseModel("获取聊天记录失败失败");
+        return ResponseModel.getCommonSuccessResponseModel(result);
+    }
+
+    @At("/getReMsg")
+    @POST
+    public ResponseModel getReMsg(Message message) {
         List<Message> result = dao.query(Message.class, Cnd.where("sender", "=", message.getSender()).and("receiver","=",message.getReceiver()));
         for (int i = 0; i < result.size(); i++) {
             //接受方
