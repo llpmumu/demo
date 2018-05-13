8a2732dcaafa39339d4f14346d777858758bdbb5
llpmumu <824484941@qq.com>
2018-05-14 01:13:14 +0800

    .......

--- src/main/java/com/manshop/bean/Tag.java
+++ src/main/java/com/manshop/bean/Tag.java
@@ -1,4 +1,21 @@
 package com.manshop.bean;
 
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("t_tag")
 public class Tag {
+    @Name
+    private String tag;
+
+    public Tag() { }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
